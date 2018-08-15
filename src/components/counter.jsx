afbbf3579a9a885a5669d701635423ff6341e3f1
Anna Grochola <agrochola@cyfrowypolsat.pl>
2018-08-15 20:24:21 +0200

    Modify classes for badge - 51min

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,19 +2,26 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 1
   };
 
   render() {
+
     return (
       <div>
-        <span className="badge badge-warning m-2">
+        <span className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
 
   formatCount() {
     const { count } = this.state;
