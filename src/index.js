935214bb5ef44fb537c9a06d9067431aaaecc44f
Anna Grochola <agrochola@cyfrowypolsat.pl>
2018-08-15 20:13:24 +0200

    Working draft, styles removed - 47min

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Counter from "./components/counter";
+import registerServiceWorker from "./registerServiceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+
+ReactDOM.render(<Counter />, document.getElementById("root"));
+registerServiceWorker();
