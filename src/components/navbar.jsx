fdbd8aec18832e280b2e2eca2436bba03ea835b9
Anna Grochola <agrochola@cyfrowypolsat.pl>
2018-08-20 01:32:32 +0200

    Add navbar - 2h

---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    render() { 
+        return ( 
+            <nav className="navbar navbar-light bg-light">
+              <a 
+                className="navbar-brand" 
+                href="#">
+                Navbar
+              </a>
+            </nav>
+        );
+    }
+}
+ 
+export default NavBar;<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+</nav>
